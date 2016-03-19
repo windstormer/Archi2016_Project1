a8e810d0d0ff7e8116074b3b54d2d3cf3b787f1f
windstormer <chenjuzen@gmail.com>
2016-03-20 00:50:43 +0800

    add comment and cut all the instruction and data in memory

--- 103062137_01/simulator/main.c
+++ 103062137_01/simulator/main.c
@@ -3,18 +3,29 @@
 
 
 
-char reg[128];
+int reg[32];
 int PC;
 unsigned char ii[1024];
 unsigned char di[1024];
 
-/*
+int iim[256];
+unsigned char dim[256];
+
+
 int combine(unsigned char a,unsigned char b,unsigned char c,unsigned char d)
 {
 
+int back=0;
+back |= a;
+back <<= 8;
+back |= b;
+back <<= 8;
+back |= c;
+back <<= 8;
+back |= d;
+return back;
 
-}*/
-
+}
 
 int main(void)
 {
@@ -25,12 +36,15 @@ FILE *dimage = fopen("../../archiTA/testcase/open_testcase/func/dimage.bin","rb"
 
 
 long sdimage=0,siimage=0;
+int sdata=0,sins=0;
 
 int i,j;
 
 memset(reg,0,sizeof(reg));
 memset(ii,0,sizeof(ii));
 memset(di,0,sizeof(di));
+memset(iim,0,sizeof(iim));
+memset(dim,0,sizeof(dim));
 PC=0;
 
 fseek(iimage , 0 , SEEK_END);
@@ -48,34 +62,39 @@ fseek(dimage , 0 , SEEK_END);
 fread(di,sizeof(unsigned char),sdimage,dimage);
 
 /*
-for(i=0;i<sdimage;i++)
-printf("%x\n",di[i]);
+for(i=0;i<siimage;i++)
+printf("%x\n",ii[i]);
 */
+reg[29]=combine(di[0],di[1],di[2],di[3]);
+sdata=combine(di[4],di[5],di[6],di[7]);
 
+for(i=0;i<sdata*4;i++)
+{
+    dim[i]=di[8+i];
+}
+
+PC=combine(ii[0],ii[1],ii[2],ii[3]);
+sins=combine(ii[4],ii[5],ii[6],ii[7]);
+
+for(i=0;i<sins;i++)
+{
+    iim[i]=combine(ii[8+i*4],ii[9+i*4],ii[10+i*4],ii[11+i*4]);
+}
 /*
-fread(di[1],1,1,dimage);
-fread(di[2],1,1,dimage);
-fread(di[3],1,1,dimage);
-
-printf("%c %c %c %c\n",di[0],di[1],di[2],di[3]);
-
-reg[29]=di[0];
-fscanf(dimage,"%d",&di[1]);
-for(int i=0;i<di[1];i++)
-{
-    fscanf(dimage,"%d",&di[i+2]);
-}
-fscanf(iimage,"%d",&ii[0]);
-PC=ii[0];
-fscanf(iimage,"%d",&ii[1]);
-for(int i=0;i<ii[1];i++)
-{
-    fscanf(iimage,"%d",&ii[i+2]);
-}
-
-printf("ii[0]= %d\n",ii[0]);
+for(i=0;i<sins;i++)
+printf("%x\n",iim[i]);
 */
 
+/**
+ii : string of all the data int iimage.bin
+di : string of all the data int dimage.bin
+PC : current PC
+reg : int array of all the register
+iim : int array of all the instruction
+dim : unsigned char array of all the data in memory
+sins: int of the number of instruction
+sdata : int of the number of memory (word)
+**/
 
 return 0;
 }
